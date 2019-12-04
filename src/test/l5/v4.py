e34e1f3e7a23c237ac260f5add93ad187d07a665
kharesp <shweta.p.khare@gmail.com>
2019-12-04 13:50:23 -0600

    l5 correction

--- src/test/l5/v4.py
+++ src/test/l5/v4.py
@@ -4,7 +4,7 @@ sys.path.append('src/vertices')
 import vertex,sink
 
 v4=sink.Sink('4',
-  vertex.Graph('l4',{
+  vertex.Graph('l5',{
     '0':vertex.ZmqConnector('192.168.88.88',5555),
     '1':vertex.ZmqConnector('192.168.88.88',6666),
     '2':vertex.ZmqConnector('192.168.88.88',7777),
