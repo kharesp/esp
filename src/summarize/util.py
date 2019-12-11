313660a99fe0c2b36e8bed7827903e35bc925293
VUZhuangweiKang <zhuangwei.kang@vanderbilt.edu>
2019-12-11 15:12:09 -0600

    working automated test deployment

--- src/summarize/util.py
+++ src/summarize/util.py
@@ -29,7 +29,7 @@ def process_util(log_dir):
     host_util_map[hostname]= {'cpu':avg_cpu,\
       'iowait':avg_iowait,\
       'mem':avg_mem,\
-      'systat_mem':np.mean(data['per'])}
+      'sysstat_mem':np.mean(data['per'])}
   return host_util_map
 
 #Returns a map from hostname to nw utilization: {hostname: kB/s}      
@@ -48,18 +48,18 @@ def process_nw(log_dir):
     hostname=f.rpartition('/')[2].split('_')[1].split('.')[0]
     data=pd.read_csv(f,delimiter=';',header=None,skiprows=1)
     idx=data.columns[(data=='eth0').all()][0]
-
+    
     #add average network usage to host mapping- rxkB/s+txkB/s
     host_nw_map[hostname]=np.mean(data.iloc[:,idx+3]+data.iloc[:,idx+4])
   return host_nw_map
 
 #Summarizes per-host cpu,memory and nw utilization 
 def process(log_dir):
-  if not os.path.exists():
+  if not os.path.exists('%s/summary'%(log_dir)):
     os.makedirs('%s/summary'%(log_dir))
 
-  host_util_map=process_util(log_dir)
-  host_nw_map=process_nw(log_dir)
+  host_util_map=process_util('%s/util'%(log_dir))
+  host_nw_map=process_nw('%s/util'%(log_dir))
 
   with open('%s/summary/summary_util.csv'%(log_dir),'w') as f:
     f.write('hostname,avg_cpu(%),avg_iowait(%),avg_mem(%),avg_mem_systat(%),avg_nw(kB/sec)\n')
