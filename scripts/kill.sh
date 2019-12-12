410b17c0bee6aa79d19c7634b7feb672701c217e
kharesp <kharesp28@gmail.com>
2019-12-12 17:21:40 -0600

    also remove sysstat monitor

--- scripts/kill.sh
+++ scripts/kill.sh
@@ -5,3 +5,9 @@ do
   pid=`echo $op | awk '{print $2}'`
   kill -9  $pid
 done
+
+ps -ef | grep -v grep | grep -v kill.yml | grep -w sysstat.py  | while read -r op; 
+do
+  pid=`echo $op | awk '{print $2}'`
+  kill -9  $pid
+done
