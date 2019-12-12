4b219b2268c1d0ec006649a4f782b3c4818d48ac
VUZhuangweiKang <zhuangwei.kang@vanderbilt.edu>
2019-12-12 15:24:34 -0600

    correction to send email script

--- src/experiment/send_email.py
+++ src/experiment/send_email.py
@@ -6,6 +6,7 @@ def monitor(email_id,app_password,log_dirs):
 
   for log_dir in log_dirs:
     dir_count[log_dir]=len(os.listdir(log_dir))
+  print(dir_count)
 
   s = smtplib.SMTP('smtp.gmail.com', 587) 
   # start TLS for security 
@@ -16,22 +17,25 @@ def monitor(email_id,app_password,log_dirs):
   iteration=0 
   while(any(dir_status.values())):
     iteration+=1
+    time.sleep(1800)
     for log_dir,status in dir_status.items():
       if status:
         updated_count=len(os.listdir(log_dir))
         if (updated_count==dir_count[log_dir]):
-          print('Test:%s has failed'%(os.path.basedir(log_dir)))
+          print('Test:%s has failed'%(os.path.basename(log_dir)))
           dir_status[log_dir]=False
-          s.sendmail(email_id,email_id,'Subject: Test failed:{}\n\n{Please check.}'.format(os.path.basedir(log_dir)))
+          s.sendmail(email_id,email_id,\
+            'Subject: Test failed:{}\n\nPlease check.'.format(os.path.basename(log_dir)))
         else:
           dir_count[log_dir]=updated_count
-    time.sleep(1800)
+          print('Updated test count for %s to %d'%(os.path.basename(log_dir),updated_count))
     if (iteration%2)==0:
       update_msg=''
       for log_dir,status in dir_status.items():
-        if status:
+        if True:
           updated_count=len(os.listdir(log_dir))
           update_msg+='test dir:%s count:%d\n'%(os.path.basename(log_dir),updated_count)
+      print('Sending update:\n%s'%(update_msg))
       s.sendmail(email_id,email_id,'Subject: Test Update\n\n{}'.format(update_msg))
   s.quit()
 
