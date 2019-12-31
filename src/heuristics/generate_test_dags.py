b952ee4c8dc0a284b667b950fc69dd40c77e7627
kharesp <shweta.p.khare@gmail.com>
2019-12-31 16:06:39 -0600

    app2 for testing

--- src/heuristics/generate_test_dags.py
+++ src/heuristics/generate_test_dags.py
@@ -1,4 +1,4 @@
-def create_repr(num_video_cams,output_dir):
+def create_repr1(num_video_cams,output_dir):
   with open('%s/g%d.txt'%(output_dir,num_video_cams),'w') as outf:
     outf.write('vid;vtype;upstream\n')
     outf.write('v0;src;\n')
@@ -20,5 +20,22 @@ def create_repr(num_video_cams,output_dir):
       tail_vertices.extend(['v%d'%(vnum+11),'v%d'%(vnum+13)])
     outf.write('v%d;snk;%s\n'%(13*num_video_cams+1,','.join(tail_vertices)))
 
-for vnum in range(3,7):
-  create_repr(vnum,'dags/app')
+def create_repr2(num_video_cams,output_dir):
+  with open('%s/g%d.txt'%(output_dir,num_video_cams),'w') as outf:
+    outf.write('vid;vtype;upstream\n')
+    outf.write('v0;src;\n')
+    tail_vertices=[] 
+    for vnum in [8*x for x in range(num_video_cams)]:
+      outf.write('v%d;bf;v0\n'%(vnum+1))
+      outf.write('v%d;lpr;v%d\n'%(vnum+2,vnum+1))
+      outf.write('v%d;eqh;v0\n'%(vnum+3))
+      outf.write('v%d;lpr;v%d\n'%(vnum+4,vnum+3))
+      outf.write('v%d;clahe;v0\n'%(vnum+5))
+      outf.write('v%d;lpr;v%d\n'%(vnum+6,vnum+5))
+      outf.write('v%d;seg;v%d,v%d,v%d\n'%(vnum+7,vnum+2,vnum+4,vnum+6))
+      outf.write('v%d;fib;v%d\n'%(vnum+8,vnum+7))
+      tail_vertices.append('v%d'%(vnum+8))
+    outf.write('v%d;snk;%s\n'%(8*num_video_cams+1,','.join(tail_vertices)))
+
+for vnum in range(1,5):
+  create_repr2(vnum,'dags/app2')
